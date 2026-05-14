6fdb3310495b73b499c5b0c4ead8e1673a99c06c
Param347 <paramradadiya76@gmail.com>
2026-05-14 21:04:14 +0530

    learn variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+// const ne chanj na kari saku
+const accountId=12345
+// let ne chanj kari sakiye but ano scope block hoy atlamaj kari sakiye
+let accountEmail="param@gmail.com"
+// var ne chanj kari sakiye aakha code ma game tya channj kari sakiye tethi var use karvo nahi
+var accountCity="rajkot"
+var accountPassword="2345"
+
+// accoutID=2 not allowed
+
+accountPassword="23456"
+accountCity="surat"
+// akk variable print karava mate
+console.log(accountCity)
+//akk karta vadhu variable print karava mate
+console.table([accountId,accountEmail,accountCity])
